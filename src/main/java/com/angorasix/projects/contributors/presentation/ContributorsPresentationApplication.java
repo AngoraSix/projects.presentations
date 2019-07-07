08f64c2735a4e1776e757e7a9bb0c13bfe307d5b
Gerardo Roza <rozagerardo@gmail.com>
2019-07-06 22:55:24 -0300

    Inital project setup using: * Eureka Client * Spring Boot 2 * Spring Web

---
+++ src/main/java/com/angorasix/projects/contributors/presentation/ContributorsPresentationApplication.java
@@ -0,0 +1,13 @@
+package com.angorasix.projects.contributors.presentation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContributorsPresentationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContributorsPresentationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/angorasix/projects/contributors/presentation/ContributorsPresentationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.angorasix.projects.contributors.presentation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ContributorsPresentationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
