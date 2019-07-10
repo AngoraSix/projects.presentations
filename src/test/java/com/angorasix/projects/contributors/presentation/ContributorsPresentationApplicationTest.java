a638bbb8c00c185b6882eb4a30e5302011adca5b
Gerardo Roza <rozagerardo@gmail.com>
2019-07-10 13:42:34 -0300

    Configuring basic project: * using parent pom * docker configurations * docker-compose configurations * surefire configuration

--- src/test/java/com/angorasix/projects/contributors/presentation/ContributorsPresentationApplicationTest.java
+++ src/test/java/com/angorasix/projects/contributors/presentation/ContributorsPresentationApplicationTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ContributorsPresentationApplicationTests {
+public class ContributorsPresentationApplicationTest {
 
 	@Test
 	public void contextLoads() {
