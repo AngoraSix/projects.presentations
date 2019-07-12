8ed09ea42c7ee55f2cfeda1066e1cf776dedfdd7
Gerardo Roza <rozagerardo@gmail.com>
2019-07-12 16:02:04 -0300

    Added CI script, and modified project to fail

--- src/main/java/com/angorasix/projects/contributors/presentation/web/controllers/ProjectRestController.java
+++ src/main/java/com/angorasix/projects/contributors/presentation/web/controllers/ProjectRestController.java
@@ -4,13 +4,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * RestController containing Project endpoints.
+ * 
+ * @author rozagerardo
+ *
+ */
 @RestController
 @RequestMapping("projects")
 public class ProjectRestController {
 
-    @GetMapping
-    public String getAllProjects() {
-        return "TODO - projects";
-    }
+  @GetMapping
+  public String getAllProjects() {
+    return "TODO - projects";
+  }
 
 }
---
+++ src/test/java/com/angorasix/projects/contributors/presentation/web/controllers/ProjectRestControllerIntegrationTest.java
@@ -0,0 +1,21 @@
+package com.angorasix.projects.contributors.presentation.web.controllers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(ProjectRestController.class)
+public class ProjectRestControllerIntegrationTest {
+
+  @Autowired
+  MockMvc mockMvc;
+
+  @Test
+  public void cicd_test() throws Exception {
+    mockMvc.perform(get("/projects")).andExpect(content().string("GER - WRONG!"));
+  }
+
+}
